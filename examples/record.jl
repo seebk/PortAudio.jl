6f6ab212530470276a5731d1742fbf738f8e4710
Sebastian Kraft <skraft@hsu-hh.de>
2016-01-30 18:45:36 +0100

    Add example of playrec() usage

--- examples/record.jl
+++ examples/record.jl
@@ -11,9 +11,18 @@ devID = convert(PaDeviceIndex, -1)
 # or retrieve a specific device by name
 #devID = find_portaudio_device("default")
 
-# open stream and play the audio
+# open stream and record a few seconds
 stream = open(devID, (2, 0), sample_rate, buf_size)
-x = read(stream, 2*sample_rate)
+z = read(stream, 2*sample_rate)
+close(stream)
+
+# open a duplex stream (not supported on all host API...)
+stream = open(devID, (2, 2), sample_rate, buf_size)
+# play some noise and record it at the same time
+x = convert(Array{Float32}, randn(MersenneTwister(),sample_rate*2,2))
+x = x ./ 10
+y = playrec(stream, x)
+# close the stream
 close(stream)
 
 Pa_Terminate()
