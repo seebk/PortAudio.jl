1622833b7ca4fa4eca8cc4953db974586b581648
Sebastian Kraft <skraft@hsu-hh.de>
2016-02-07 18:08:10 +0100

    Add first basic tests, mainly interleaving/deinterleaving

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,25 @@
 using PortAudio
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+PortAudio.initialize()
+
+# print list of all available devices
+PortAudio.list_devices()
+
+# retrieve list of all available devices
+devices = PortAudio.get_devices()
+
+# test interleaving/deinterleaving
+num_frames   = 512
+for num_channels = 1:6
+    print("Interleaving/deinterleaving $num_channels channels and $num_frames frames... ")
+    buf_deint  = randn(MersenneTwister(), num_frames, num_channels)
+    buf_int    = zeros(num_frames*num_channels)
+    buf_res    = zeros(buf_deint)
+    PortAudio.interleave(buf_deint, buf_int, num_channels, num_frames)
+    PortAudio.deinterleave(buf_int, buf_res, num_channels, num_frames)
+    @test buf_deint == buf_res
+    println("OK")
+end
+
+PortAudio.terminate()
